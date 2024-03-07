6654a6afb095a8c150db3f85df3a17fe80f6b0cc
casperpersson <persson1201@gmail.com>
2024-03-07 11:37:12 +0100

    First commit test

---
+++ Lists.py
@@ -0,0 +1,15 @@
+numbers = []
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+strings = []
+strings.append("Hello")
+strings.append("World")
+names = ["John", "Eric", "Jessica"]
+
+second_name = names[1]
+
+
+print(numbers)
+print(strings)
+print("the second name on the names list is %s" %second_name)
